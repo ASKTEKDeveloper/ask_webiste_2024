ad8a2ffefc7bb7d02607092699bba585c51aaca1
David-askteck <davidraja.askteck@gmail.com>
2025-01-28 12:20:47 +0530

    object property added issue

--- pages/operation_bulletin.js
+++ pages/operation_bulletin.js
@@ -117,7 +117,7 @@ const ProjectGrid = () => {
 <p><strong>Phone Number:</strong> ${datas.phone_number}</p>
 <p><strong>Company Name:</strong> ${datas.company_name}</p>
 <p><strong>City:</strong> ${datas.city}</p>
-<p><strong>City:</strong> ${datas.country}</p>
+<p><strong>City:</strong> ${datas.country.label}</p>
 <p>Please take note of this and follow up with the user if necessary.</p>
 <p>Best regards,</p>
 <p>ASK TECHNOLOGY</p>
